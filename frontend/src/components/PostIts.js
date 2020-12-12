19690741e5651b71c03c3022f26630b7b38d6da9
popkorn96 <mpaloma2014@gmail.com>
2020-12-12 14:31:39 -0600

    created components files

---
+++ frontend/src/components/PostIts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PostIts() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ frontend/src/components/SavedStories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SavedStories() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
