8d85fa23b66572d874a61c2141c275c4e55d2f05
popkorn96 <mpaloma2014@gmail.com>
2020-12-14 11:30:40 -0600

    set up login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -9,8 +11,44 @@ import SavedStories from './components/SavedStories';
 import Account from './containers/Account'
 import { NavigationBar } from './components/NavigationBar';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  }
+componentDidMount() {
+  this.loginStatus()
+}
+loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', 
+    {withCredentials: true})    
+.then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
+handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
+  
+  render() {
+    return(
     <Router>
       <NavigationBar />
       <Switch>
@@ -21,7 +59,7 @@ function App() {
         <Route path="/account" component={Account}/>
       </Switch>
     </Router>
-  );
+    )};
 }
 
 export default App;
---
+++ frontend/src/components/LogIn.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errors: ''
+     };
+  }
+handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  };
+handleSubmit = (event) => {
+    event.preventDefault()
+  };
+render() {
+    const {email, password} = this.statereturn (
+      <div>
+        <h1>Log In</h1>        
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="email"
+            type="text"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            placeholder="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />         
+        <button placeholder="submit" type="submit">
+            Log In
+          </button>          
+          <div>
+            or <Link to='/signup'>Sign Up</Link>
+          </div>
+          
+         </form>
+      </div>
+    );
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const {email, password} = this.state
+    let user = {
+      email: email,
+      password: password
+    }
+        
+    axios.post('http://localhost:3001/login', {user}, {withCredentials: true})
+        .then(response => {
+        if (response.data.logged_in) {
+            this.props.handleLogin(response.data)
+            this.redirect()
+        } else {
+            this.setState({
+            errors: response.data.errors
+            })
+        }
+        })
+        .catch(error => console.log('api errors:', error))
+    };
+    redirect = () => {
+        this.props.history.push('/')
+    }
+    handleErrors = () => {
+        return (
+        <div>
+            <ul>
+            {this.state.errors.map(error => {
+            return <li key={error}>{error}</li>
+            })}
+            </ul>
+        </div>
+        )
+    };
+}
+export default Login;
