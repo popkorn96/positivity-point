a8a29298f4da908bc75f9e6a33a6da764ead600a
popkorn96 <mpaloma2014@gmail.com>
2020-12-18 15:38:25 -0600

    completed user-filter

--- frontend/src/components/Goals.js
+++ frontend/src/components/Goals.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {getGoals} from '../redux/actions/goalActions';
+import {sessionStatus} from '../redux/actions/sessionStatus'
 import GoalListItem from '../items/GoalListItem';
 
 class Goals extends Component {
     componentDidMount() {
         this.props.getGoals();
+        this.props.sessionStatus();
     };
     render(){
+        var props = this.props
         return (
             <div>
                 <h1 class="text-center">Goals</h1>
+                <h1>{this.props.userState.name}</h1>
                 <button type="button" class="btn btn-outline-primary btn-lg btn-block" >Create New Goal</button><hr></hr>
-                <GoalListItem goals={this.props.goals}/>
+                {this.props.goals.filter(function(goal, i){
+                    return goal.user_id === props.userState.id
+                })
+                .map((goal, i) =>
+                    <GoalListItem key={goal.id} goal={goal} user={props.userState}/>
+                )}.bind(this))
+                
             </div>
         )
     }
 }
-
-export default connect(({goals}) => ({ goals: goals.all }), {getGoals})(Goals)
+const mapStateToProps = state => {
+    return {
+        userState: state.userState.user,
+        goals: state.goals.all,
+    };
+};
+const mapDispatchToProps={
+    sessionStatus,
+    getGoals
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Goals)
--- frontend/src/items/GoalListItem.js
+++ frontend/src/items/GoalListItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
 
-export default function goalListItem({goals}) {
+export default function goalListItem({goal, user}) {
     return (
-        <ListGroup variant="flush">{goals.map((goal, i) => 
-        <ListGroup.Item key={goal.id}>{goal.content}</ListGroup.Item>)}
+        <div>
+            <h1>{user.name}</h1>
+        <ListGroup >
+            <ListGroup.Item key={goal.user_id}><strong>{goal.title}</strong><br></br>{goal.content}</ListGroup.Item>
+            <Button>Remove</Button>
         </ListGroup>
+        </div>
     )
 }
